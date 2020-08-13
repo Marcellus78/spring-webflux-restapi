d8eb5ef5fe9845f73c4028b6faec94d558381d2d
marcellus <War13win23@>
2020-08-13 09:06:29 +0200

    First commit

---
+++ src/main/java/com/marcellus/springwebfluxrestapi/SpringWebfluxRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.marcellus.springwebfluxrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcellus/springwebfluxrestapi/SpringWebfluxRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcellus.springwebfluxrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
